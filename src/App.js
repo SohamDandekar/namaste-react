36e2da8a04e33afe4fec071497ab8c7cc91b822c
SohamDandekar <dandekarsoham274@gmail.com>
2026-02-01 16:39:08 +0530

    episode-05 fixed issues 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
-import Header from "./src/components/Header";
-import Body from "./src/components/Body"; 
+import Header from "./components/Header";
+import Body from "./components/Body"; 
 
 
 const AppLayout = () => {
