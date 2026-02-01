089ac7b87291600fccb4fe55fc0539f2c978413f
SohamDandekar <dandekarsoham274@gmail.com>
2026-02-01 11:54:36 +0530

    episode-05

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,23 @@
+import { CDN_URL } from "../utils/constant";
+
+const RestaurantCard = (props) => {
+    const { resData } = props;
+
+    const { name, cuisines, avgRating, costForTwo, cloudinaryImageId, sla } = resData?.info;
+    const { deliveryTime } = sla;
+
+    return (
+        <div className="res-card" style={{backgroundColor:"lightblue"}}>
+            <img src={CDN_URL + cloudinaryImageId}
+            alt="res-logo"
+            className="res-logo"/>
+            <h3>{name}</h3>
+            <h4>{cuisines.join()}</h4>
+            <h4>{deliveryTime} Mins</h4>
+            <h4>{avgRating} Stars</h4>
+            <h4>{costForTwo}</h4>
+        </div>
+    )
+};
+
+export default RestaurantCard;
---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjDMszOev5meVUs0m-AF8BOYHncIJYhfgg0A&s"
+
+const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+
+export {LOGO_URL,CDN_URL};
